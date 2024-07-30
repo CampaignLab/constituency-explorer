e017c0427f9e36053a751c11235a411130d8434d
Ryan Chandler <ryangjchandler@gmail.com>
2024-07-30 16:15:48 +0100

    wip

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
 
-        <title>Constituency Explorer</title>
+        <x-layouts.meta />
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
---
+++ resources/views/components/layouts/meta.blade.php
@@ -0,0 +1,17 @@
+<title>Constituency Explorer</title>
+<meta name="title" content="Constituency Explorer" />
+<meta name="description" content="A tool to help you run local campaigns with local data." />
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website" />
+<meta property="og:url" content="{{ url()->current() }}" />
+<meta property="og:title" content="Constituency Explorer" />
+<meta property="og:description" content="A tool to help you run local campaigns with local data." />
+<meta property="og:image" content="{{ asset('og.png') }}" />
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image" />
+<meta property="twitter:url" content="{{ url()->current() }}" />
+<meta property="twitter:title" content="Constituency Explorer" />
+<meta property="twitter:description" content="A tool to help you run local campaigns with local data." />
+<meta property="twitter:image" content="{{ asset('og.png') }}" />
