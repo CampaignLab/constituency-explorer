511beb276d5029e92c722a9cf2054a28f4d55848
Ryan Chandler <ryangjchandler@gmail.com>
2024-08-15 10:54:48 +0100

    wip

--- resources/views/components/constituency/overview-entity-card.blade.php
+++ resources/views/components/constituency/overview-entity-card.blade.php
@@ -20,23 +20,21 @@
 
     <div class="border border-primary-border rounded-b-lg divide-y divide-primary-border text-sm">
         <div class="p-4 flex items-center justify-between">
-            <p class="font-bold">Population Overlap</p>
-            <p>{{ number_format($populationOverlap) }}</p>
-        </div>
-
-        <div class="p-4 flex items-center justify-between">
-            <p class="font-bold">Area Overlap</p>
-            <p>{{ number_format($areaOverlap) }}</p>
-        </div>
-
-        <div class="p-4 flex items-center justify-between">
             <p class="font-bold">Population Overlap Percentage</p>
-            <p>{{ number_format($populationOverlapPercentage) }}%</p>
+
+            <div class="text-right">
+                <p>{{ number_format($populationOverlapPercentage) }}%</p>
+                <p class="text-xs text-muted-foreground">({{ number_format($populationOverlap) }})</p>
+            </div>
         </div>
 
         <div class="p-4 flex items-center justify-between">
             <p class="font-bold">Area Overlap Percentage</p>
-            <p>{{ number_format($areaOverlapPercentage) }}%</p>
+
+            <div class="text-right">
+                <p>{{ number_format($areaOverlapPercentage) }}%</p>
+                <p class="text-xs text-muted-foreground">({{ number_format($areaOverlap) }})</p>
+            </div>
         </div>
     </div>
 </div>
